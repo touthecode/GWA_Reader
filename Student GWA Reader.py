96f0a50e2600f5bcff68f20278d66cb274ea10c8
touthecode <130022845+touthecode@users.noreply.github.com>
2023-05-26 01:22:26 +0800

    updated list, added line split, fixed typos

--- Student GWA Reader.py
+++ Student GWA Reader.py
@@ -1,2 +1,15 @@
 import sys
 
+# Make a txt file with a list of students with their respective grades
+# Highest is 1.00 while lowest is 5.00
+with open("C:/Users/Tou/Desktop/School/OOPPython Stuff/GWA/master_list.txt") as gwa_reader:
+    students_stats = gwa_reader.readlines()
+    top_student_name = ""
+    top_student_grade = ""
+
+    topmost_grade = 1.00
+
+    # Separate the name from the grade in the text file
+    for line in students_stats:
+        student_name, student_grade = line.split("| ")
+        student_grade = float(student_grade)
